4aff71c2819089f94a8d1741dbbf36077f3a2983
Senko Rasic <senko.rasic@dobarkod.hr>
2014-05-04 11:25:29 +0200

    add stormpath_cli.util with config file helpers
    Things like loading and saving a config file, or getting a config
    file path (where the config home is ~/.stormpath/cli, expanded
    each time).

---
+++ stormpath_cli/util.py
@@ -0,0 +1,28 @@
+from os import environ, makedirs, rename, chmod
+from os.path import join, dirname, exists
+
+
+def get_config_path(name):
+    return join(environ.get('HOME', '/'), '.stormpath', 'cli', name)
+
+
+def store_config_file(name, data):
+    fpath = get_config_path(name)
+
+    if not exists(dirname(fpath)):
+        makedirs(dirname(fpath), 0700)
+
+    tmp = fpath + '.tmp'
+    with open(tmp, 'w') as fd:
+        fd.write(data)
+        chmod(tmp, 0400)
+    rename(tmp, fpath)
+
+
+def get_config_file(name, default=None):
+    fpath = get_config_path(name)
+
+    if exists(fpath):
+        return open(fpath, 'r').read()
+    else:
+        return default
