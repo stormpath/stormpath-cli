c23b7bd79397ddc773a1978ae253d6de6b27c64f
Deni Bertovic <deni@kset.org>
2014-05-13 16:04:53 +0200

    refactor actions tests
    only test the functions that actually do the work

--- tests/test_actions.py
+++ tests/test_actions.py
@@ -5,105 +5,59 @@ try:
 except ImportError:
     from unittest.mock import create_autospec, MagicMock
 
-from stormpath_cli import resources, actions
-from stormpath_cli import output
-from stormpath.client import Client
-from stormpath.data_store import DataStore
+from stormpath_cli import actions
 from stormpath.resources.account import AccountList
 from stormpath.resources.application import ApplicationList
-from stormpath.resources.directory import DirectoryList
-from stormpath.resources.group import GroupList
+
 
 class TestActions(unittest.TestCase):
 
-    def test_application_list_action(self):
-        ds = create_autospec(DataStore)
-        ds.get_resource.return_value = {
-            'href': 'test/resource',
-            'offset': 2,
-            'limit': 25,
-            'items': [
-                {'href': 'test/resource'},
-            ]
-        }
-        resource = ApplicationList(client=MagicMock(data_store=ds), properties={
-            'href': '/',
-            'offset': 0,
-            'limit': 25,
-            'items': [
-                {'href': 'test/resource'},
-            ]
-        })
+    def test_specialized_query_gets_attributes_from_options(self):
+        coll = AccountList(MagicMock(), href="test/resource")
+        maps = {AccountList: {'attrname': '--foo'}}
+        args = {'--foo': 'FOO', '--bar': 'BAR'}
 
-        arguments = {'<resource>': 'application', '<action>': 'list'}
-        actions.list_resources(resource, arguments)
-        ds.get_resource.assert_called_with('test/resource')
+        ret = actions._specialized_query(coll, args, maps)
+        self.assertEqual(ret, {'attrname': 'FOO'})
 
-    def test_account_list_action(self):
-        ds = create_autospec(DataStore)
-        ds.get_resource.return_value = {
-            'href': 'test/resource',
-            'offset': 2,
-            'limit': 25,
-            'items': [
-                {'href': 'test/resource'},
-            ]
-        }
-        resource = AccountList(client=MagicMock(data_store=ds), properties={
-            'href': '/',
-            'offset': 0,
-            'limit': 25,
-            'items': [
-                {'href': 'test/resource'},
-            ]
-        })
+    def test_specialized_query_loads_json_if_specified(self):
+        coll = AccountList(MagicMock(), href="test/resource")
+        maps = {AccountList: {'attrname': '--foo'}}
+        args = {'--json': """{"attrname": "FOO"}"""}
 
-        arguments = {'<resource>': 'account', '<action>': 'list'}
-        actions.list_resources(resource, arguments)
-        ds.get_resource.assert_called_with('test/resource')
+        ret = actions._specialized_query(coll, args, maps)
+        self.assertEqual(ret, {'attrname': 'FOO'})
 
-    def test_group_list_action(self):
-        ds = create_autospec(DataStore)
-        ds.get_resource.return_value = {
-            'href': 'test/resource',
-            'offset': 2,
-            'limit': 25,
-            'items': [
-                {'href': 'test/resource'},
-            ]
-        }
-        resource = GroupList(client=MagicMock(data_store=ds), properties={
-            'href': '/',
-            'offset': 0,
-            'limit': 25,
-            'items': [
-                {'href': 'test/resource'},
-            ]
-        })
+    def test_that_resource_identifiers_are_required_and_parsed_properly(self):
+        coll = ApplicationList(MagicMock(), href="test/resource")
+        attrs = {'name': 'Test Application'}
 
-        arguments = {'<resource>': 'group', '<action>': 'list'}
-        actions.list_resources(resource, arguments)
-        ds.get_resource.assert_called_with('test/resource')
+        ret = actions._primary_attribute(coll, attrs)
+        self.assertEqual(ret, ('name', 'Test Application'))
 
-    def test_directory_list_action(self):
-        ds = create_autospec(DataStore)
-        ds.get_resource.return_value = {
-            'href': 'test/resource',
-            'offset': 2,
-            'limit': 25,
-            'items': [
-                {'href': 'test/resource'},
-            ]
-        }
-        resource = DirectoryList(client=MagicMock(data_store=ds), properties={
-            'href': '/',
-            'offset': 0,
-            'limit': 25,
-            'items': [
-                {'href': 'test/resource'},
-            ]
-        })
+    def test_gathering_resource_attributes(self):
+        coll = ApplicationList(MagicMock(), href="test/resource")
+        args = {'<attributes>': ['name=test', 'description=test']}
+        ret = actions._gather_resource_attributes(coll, args)
+        self.assertEqual(ret, {
+            '--name': 'test',
+            '--description': 'test',
+            '<attributes>': ['name=test', 'description=test']})
 
-        arguments = {'<resource>': 'directory', '<action>': 'list'}
-        actions.list_resources(resource, arguments)
-        ds.get_resource.assert_called_with('test/resource')
+    def test_gathering_resource_attributes_raises_error_on_bad_syntax(self):
+        coll = ApplicationList(MagicMock(), href="test/resource")
+        args = {'<attributes>': ['no_eq_sign', 'description=test']}
+        self.assertRaises(ValueError, actions._gather_resource_attributes, coll, args)
+        args = {'<attributes>': ['no_eq_sign=', 'description=']}
+        self.assertRaises(ValueError, actions._gather_resource_attributes, coll, args)
+
+    def test_only_accounts_take_the_group_flag_into_account(self):
+        args = {'--groups': 'testgroup'}
+        resource = MagicMock()
+        ret = actions._add_resource_to_groups(resource, args)
+        self.assertIsNotNone(ret)
+        resource.add_group.assert_called_once_with('testgroup')
+
+        del resource.add_group
+        ret = actions._add_resource_to_groups(resource, args)
+        self.assertIsNone(ret)
