9a00493a9ab7c4a114a32302e8a557f4be53da09
Randall Degges <r@rdegges.com>
2016-06-16 16:54:15 -0700

    Cleanup.

--- stormpath_cli/projects/java.py
+++ stormpath_cli/projects/java.py
@@ -1,3 +1,6 @@
+from os import chdir
+from subprocess import call, check_output
+
 from project import Project
 
 
@@ -11,35 +14,32 @@ class JavaProject(Project):
     deploy_args = []
 
     def download(self):
-
-        #We're going to use a temp file to keep all the java sdk files while we create sample app directory
-        #it'll get cleanuped before we're done
+        # We're going to use a temp file to keep all the java sdk files while we create sample app directory
+        # it'll get cleanuped before we're done
         temp_folder = "__stormpath-cli-java-temp"
 
-        #download the Java sdk with it's examples into the temp folder
-        subprocess.call(self.download_args + [self.remote_location, temp_folder])
+        # download the Java sdk with it's examples into the temp folder
+        call(self.download_args + [self.remote_location, temp_folder])
 
-        #make sure we're working off the latest tagged release so that the maven build later will work
-        os.chdir(temp_folder)
-        subprocess.call(['ls'])
-        tags = subprocess.check_output(['git', 'for-each-ref', '--sort=taggerdate', '--format', '%(refname)', '%(taggerdate)', 'refs/tags'])
+        # make sure we're working off the latest tagged release so that the maven build later will work
+        chdir(temp_folder)
+        call(['ls'])
+        tags = check_output(['git', 'for-each-ref', '--sort=taggerdate', '--format', '%(refname)', '%(taggerdate)', 'refs/tags'])
         tags = [line for line in tags.split('\n')]
         latest_tag = tags[len(tags) - 2]
 
-        subprocess.call(['git', 'checkout', latest_tag])
-        os.chdir('..')
+        call(['git', 'checkout', latest_tag])
+        chdir('..')
 
-        #move the sample the intended folder the user will be using
-        subprocess.call(['mkdir', self.name])
-        subprocess.call(['cp', '-R', '%s/examples/%s/'%(temp_folder, self.target_folder_name), self.name])
+        # move the sample the intended folder the user will be using
+        call(['mkdir', self.name])
+        call(['cp', '-R', '{}/examples/{}/'.format(temp_folder, self.target_folder_name), self.name])
 
-        #clean up the temp files
-        subprocess.call(['rm', '-rf', temp_folder])
+        # clean up the temp files
+        call(['rm', '-rf', temp_folder])
 
-        #prep the new sample project folder for git
-        os.chdir(self.name)
-        subprocess.call(['git', 'init'])
-        subprocess.call(['git', 'commit', '--all'])
-        os.chdir('..')
-
-    #end download
+        # prep the new sample project folder for git
+        chdir(self.name)
+        call(['git', 'init'])
+        call(['git', 'commit', '--all'])
+        chdir('..')
--- stormpath_cli/projects/node.py
+++ stormpath_cli/projects/node.py
@@ -1,10 +1,10 @@
-from project import *
+from project import Project
+
 
 class NodeProject(Project):
-
     name = 'stormpath-nodejs-sample'
     remote_location = 'https://github.com/stormpath/express-stormpath-sample-project.git'
     download_args = ['git', 'clone']
     install_args = ['npm', 'install']
-    run_args = ['node', 'server.js']
+    run_args = ['npm', 'start']
     deploy_args = []
